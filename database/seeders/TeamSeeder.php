4e874e7ad675451afbc88e69ace6f462360a2314
YanDevv - Macbook <ybritogomes@gmail.com>
2026-03-30 13:11:19 -0300

    refactor(team): added pre-defined teams of Brasileirão Série A on team's seeder

--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -11,10 +11,45 @@ class TeamSeeder extends Seeder
     use WithoutModelEvents;
 
     /**
+     * Available team names to pick from.
+     *
+     * @var array<int, string>
+     */
+    private array $teamNames = [
+        'Athletico Paranaense',
+        'Atlético-MG',
+        'Bahia',
+        'Botafogo',
+        'Chapecoense',
+        'Corinthians',
+        'Coritiba',
+        'Cruzeiro',
+        'Flamengo',
+        'Fluminense',
+        'Grêmio',
+        'Internacional',
+        'Mirassol',
+        'Palmeiras',
+        'Red Bull Bragantino',
+        'Remo',
+        'Santos',
+        'Santos',
+        'São Paulo',
+        'Vasco da Gama',
+        'Vitória',
+    ];
+
+    /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Team::factory(8)->create();
+        // Team::factory(8)->create(); //! If you want to create with random company names.
+
+        foreach ($this->teamNames as $teamName) {
+            Team::factory()->create([
+                'name' => $teamName,
+            ]);
+        };
     }
 }
