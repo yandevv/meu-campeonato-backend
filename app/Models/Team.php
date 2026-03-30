fde660d4cb35258b19a972e3f2f800184ed970a2
YanDevv - Macbook <ybritogomes@gmail.com>
2026-03-30 12:51:50 -0300

    feat(teams): changed from incremental IDs to uuidv7

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use Database\Factories\TeamFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 #[Fillable(['name'])]
 class Team extends Model
 {
+    use HasUuids;
     /** @use HasFactory<TeamFactory> */
     use HasFactory;
 }
