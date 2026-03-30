2e572a12a7a4e7219bb78cfd17b89337caeabd41
YanDevv - Macbook <ybritogomes@gmail.com>
2026-03-30 15:32:03 -0300

    feat(tournaments): implemented tournament related endpoints on api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TeamController;
+use App\Http\Controllers\TournamentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,3 +15,15 @@ Route::post('/teams', [TeamController::class, 'store'])->name('teams.store');   
 Route::get('/teams/{team}', [TeamController::class, 'show'])->name('teams.show');      // Show a specific team
 Route::put('/teams/{team}', [TeamController::class, 'update'])->name('teams.update');  // Update a specific team
 Route::delete('/teams/{team}', [TeamController::class, 'destroy'])->name('teams.destroy'); // Delete a specific team
+
+/*
+|--------------------------------------------------------------------------
+| Tournament Endpoints
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/tournaments', [TournamentController::class, 'index'])->name('tournaments.index');
+Route::post('/tournaments', [TournamentController::class, 'store'])->name('tournaments.store');
+Route::get('/tournaments/{tournament}', [TournamentController::class, 'show'])->name('tournaments.show');
+Route::put('/tournaments/{tournament}', [TournamentController::class, 'update'])->name('tournaments.update');
+Route::delete('/tournaments/{tournament}', [TournamentController::class, 'destroy'])->name('tournaments.destroy');
